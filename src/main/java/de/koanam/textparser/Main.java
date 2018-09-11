e0a0dae93d224713d7806b8891c7a3c013bf5617
Flo <Flo@DESKTOP-NGU6QN4.fritz.box>
2018-09-11 20:47:52 +0200

    Annotations added

---
+++ src/main/java/de/koanam/textparser/Main.java
@@ -0,0 +1,7 @@
+package de.koanam.textparser;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ src/main/java/de/koanam/textparser/annotations/ParsableAttribute.java
@@ -0,0 +1,20 @@
+package de.koanam.textparser.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Documented
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+public @interface ParsableAttribute {
+
+	int start();
+	
+	int length();
+	
+}
